511efc23f46f32f0a4f91007aab886f9843b2e21
Chethanpoojary17 <chethanpoojary459@gmail.com>
2021-04-19 12:05:31 +0530

    initial commit

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    id: 1,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg",
+    title: "Ikigai: The Japanese secret to a long and happy life",
+    author: "Héctor García",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51SxzAVvBfL._AC_UL200_SR200,200_.jpg",
+    title: "Word Power Made Easy ",
+    author: "Norman Lewis",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/81%2BybHMWm9L._AC_UL200_SR200,200_.jpg",
+    title: "The Richest Man in Babylon",
+    author: "George S. Clason",
+  },
+];
